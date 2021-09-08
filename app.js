0f8f977c5e79f91585b99f4ff608ea08150c3663
Priyanshi Raj <priyanshiraj2909@gmail.com>
2021-09-08 20:30:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
-         const txts=document.querySelector(".animate-text").children,
-               txtsLen=txts.length;
+         const texts=document.querySelector(".animate-text").children,
+               textsLen=texts.length;
            let index=0;
           const textInTimer=3000,
                 textOutTimer=2800;
 
          function animateText() {
-            for(let i=0; i<txtsLen; i++){
-              txts[i].classList.remove("text-in","text-out");  
+            for(let i=0; i<textsLen; i++){
+              texts[i].classList.remove("text-in","text-out");  
             }
-            txts[index].classList.add("text-in");
+            texts[index].classList.add("text-in");
 
             setTimeout(function(){
-                txts[index].classList.add("text-out");              
+                texts[index].classList.add("text-out");              
             },textOutTimer)
 
             setTimeout(function(){
 
-              if(index == txtsLen-1){
+              if(index == textsLen-1){
                   index=0;
                 }
                else{
